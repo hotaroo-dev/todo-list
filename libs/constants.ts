bdddb3f5dbaa78ca9a3c4c228ba36e64504e5c3f
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2024-04-09 14:23:09 +0700

    feat(contracts): deploy smart contracts in sepolia test network

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,4 +1,4 @@
 import TodoList from "@/smart-contract/build/contracts/TodoList.json";
 
-export const contractAddress = "0x3F2C6Db0b13a119eDA82bAbE18cBd68FbAaE10c3";
+export const contractAddress = "0xeCa915940807f8D3d32a7Aa0CF39fc3bE5691B22";
 export const contractAbi = TodoList.abi;
